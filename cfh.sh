4466acef7629a414f5097cbdab191c62c61c057e
PHC Dark  Boy <87409181+LolLloLlLolLlLolL-rgb@users.noreply.github.com>
2021-08-01 01:44:45 +0800

    Update cfh.sh

--- cfh.sh
+++ cfh.sh
@@ -12,20 +12,24 @@ echo -e "${red}Permission Denied!${NC}";
 echo "Only For Premium Users"
 exit 0
 fi
-source /root/mail.conf
-DOMAIN=$domain
-CF_ID=$email
-CF_KEY=$key
+domain=$domain
+sub=$sub
+echo -e "SUB_DOMAIN=${sub}.${domain}" >> /root/mail.conf
+SUB_DOMAIN=${sub}.${domain}
+CF_ID=${CF_ID}
+CF_KEY=${CF_KEY}
+#CF_ID=masukemailko
+#CF_KEY=masukapikeycfkokatsini
 set -euo pipefail
-read -p "Masukan Subdomain Anda :" sub
-read -p "Masukan Ip BUG :" IP
-echo "Updating DNS for ${sub}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+IP=$(wget -qO- icanhazip.com);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${domain}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${sub}" \
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
@@ -35,12 +39,16 @@ if [[ "${#RECORD}" -le 10 ]]; then
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${sub}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
 fi
 
 RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${sub}'","content":"'${IP}'","ttl":120,"proxied":false}')
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 echo "DONE...!"
+echo "Your new Domain is : ${domain}"
+echo "Your new SubDomain is : ${SUB_DOMAIN}"
+echo $domain > /root/domain
+echo $SUB_DOMAIN > /root/domain
