2ac05a92d57cb9a691964ab7611c46a370f23ad6
PHC Dark  Boy <87409181+LolLloLlLolLlLolL-rgb@users.noreply.github.com>
2021-08-01 04:05:36 +0800

    Update cubaan.sh

--- cubaan.sh
+++ cubaan.sh
@@ -53,7 +53,7 @@ echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 444, 777"  | tee -a log-install.txt
+echo "   - Stunnel4                : 444, 666"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 8181, 8080 (Proxy Sotong)"  | tee -a log-install.txt
 echo "   - Magic Proxy SSH         : 8180 (Proxy Tisu Magic)"  | tee -a log-install.txt
