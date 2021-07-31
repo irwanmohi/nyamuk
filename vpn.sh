ef814859a5176a932d90c7657f7aff4f9a74e5a9
PHC Dark  Boy <87409181+LolLloLlLolLlLolL-rgb@users.noreply.github.com>
2021-08-01 04:01:10 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -11,48 +11,45 @@ MYIP2="s/xxxxxxxxx/$MYIP/g";
 ANU=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 
 # Install OpenVPN dan Easy-RSA
-apt install openvpn easy-rsa unzip -y
-apt install openssl iptables iptables-persistent -y
-mkdir -p /etc/openvpn/server/easy-rsa/
-cd /etc/openvpn/
-wget https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/vpn.zip
-unzip vpn.zip
-rm -f vpn.zip
-chown -R root:root /etc/openvpn/server/easy-rsa/
-
+apt-get install -y openvpn
+wget -q https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/EasyRSA-3.0.8.tgz
+tar xvf EasyRSA-3.0.8.tgz
+rm EasyRSA-3.0.8.tgz
+mv EasyRSA-3.0.8 /etc/openvpn/easy-rsa
+cp /etc/openvpn/easy-rsa/vars.example /etc/openvpn/easy-rsa/vars
+sed -i 's|#set_var EASYRSA_REQ_COUNTRY\t"US"|set_var EASYRSA_REQ_COUNTRY\t"MY"|g' /etc/openvpn/easy-rsa/vars
+sed -i 's|#set_var EASYRSA_REQ_PROVINCE\t"California"|set_var EASYRSA_REQ_PROVINCE\t"Kelantan"|g' /etc/openvpn/easy-rsa/vars
+sed -i 's|#set_var EASYRSA_REQ_CITY\t"San Francisco"|set_var EASYRSA_REQ_CITY\t"Kota Bharu"|g' /etc/openvpn/easy-rsa/vars
+sed -i 's|#set_var EASYRSA_REQ_ORG\t"Copyleft Certificate Co"|set_var EASYRSA_REQ_ORG\t\t"My Team"|g' /etc/openvpn/easy-rsa/vars
+sed -i 's|#set_var EASYRSA_REQ_EMAIL\t"me@example.net"|set_var EASYRSA_REQ_EMAIL\t"akuvps@gmail.com"|g' /etc/openvpn/easy-rsa/vars
+sed -i 's|#set_var EASYRSA_REQ_OU\t\t"My Organizational Unit"|set_var EASYRSA_REQ_OU\t\t"My Team Premium Vpn"|g' /etc/openvpn/easy-rsa/vars
+sed -i 's|#set_var EASYRSA_CA_EXPIRE\t3650|set_var EASYRSA_CA_EXPIRE\t3650|g' /etc/openvpn/easy-rsa/vars
+sed -i 's|#set_var EASYRSA_CERT_EXPIRE\t825|set_var EASYRSA_CERT_EXPIRE\t3650|g' /etc/openvpn/easy-rsa/vars
+cd /etc/openvpn/easy-rsa
+./easyrsa --batch init-pki
+./easyrsa --batch build-ca nopass
+./easyrsa gen-dh
+./easyrsa build-server-full server nopass
 cd
-mkdir -p /usr/lib/openvpn/
-cp /usr/lib/x86_64-linux-gnu/openvpn/plugins/openvpn-plugin-auth-pam.so /usr/lib/openvpn/openvpn-plugin-auth-pam.so
-
-# nano /etc/default/openvpn
-sed -i 's/#AUTOSTART="all"/AUTOSTART="all"/g' /etc/default/openvpn
-
-# restart openvpn dan cek status openvpn
-systemctl enable --now openvpn-server@server-tcp-1194
-systemctl enable --now openvpn-server@server-udp-2200
-/etc/init.d/openvpn restart
-/etc/init.d/openvpn status
-
-# aktifkan ip4 forwarding
-echo 1 > /proc/sys/net/ipv4/ip_forward
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-
-# Removing all existing openvpn server files
-rm -f /etc/openvpn/server/server-tcp-1194.conf
-rm -f /etc/openvpn/client-tcp-1194.ovpn
-rm -f /home/vps/public_html/client-tcp-1194.ovpn
+mkdir /etc/openvpn/key
+cp /etc/openvpn/easy-rsa/pki/issued/server.crt /etc/openvpn/key/
+cp /etc/openvpn/easy-rsa/pki/ca.crt /etc/openvpn/key/
+cp /etc/openvpn/easy-rsa/pki/dh.pem /etc/openvpn/key/
+cp /etc/openvpn/easy-rsa/pki/private/server.key /etc/openvpn/key/
 cat > /etc/openvpn/server/server-tcp-1194.conf<<END
 port 1194
 proto tcp
 dev tun
-ca ca.crt
-cert server.crt
-key server.key
-dh dh2048.pem
+ca key/ca.crt
+cert key/server.crt
+key key/server.key
+dh key/dh.pem
 verify-client-cert none
 username-as-common-name
 key-direction 0
-plugin /etc/openvpn/plugins/openvpn-plugin-auth-pam.so login
+status /var/log/openvpn/openvpn-status.log
+log /var/log/openvpn/openvpn.log
+plugin openvpn-plugin-auth-pam.so login
 server 192.168.10.0 255.255.255.0
 ifconfig-pool-persist ipp.txt
 push "route-method exe"
@@ -63,9 +60,7 @@ user nobody
 group nogroup
 persist-key
 persist-tun
-status openvpn-status.log
-log tcp.log
-verb 2
+verb 3
 ncp-disable
 cipher none
 auth none
@@ -117,14 +112,16 @@ cat > /etc/openvpn/server/server-udp-2200.conf<<END
 port 2200
 proto udp
 dev tun
-ca ca.crt
-cert server.crt
-key server.key
-dh dh2048.pem
+ca key/ca.crt
+cert key/server.crt
+key key/server.key
+dh key/dh.pem
 verify-client-cert none
 username-as-common-name
 key-direction 0
-plugin /etc/openvpn/plugins/openvpn-plugin-auth-pam.so login
+status /var/log/openvpn/openvpn-status.log
+log /var/log/openvpn/openvpn.log
+plugin openvpn-plugin-auth-pam.so login
 server 192.168.11.0 255.255.255.0
 ifconfig-pool-persist ipp.txt
 push "route-method exe"
@@ -135,9 +132,7 @@ user nobody
 group nogroup
 persist-key
 persist-tun
-status openvpn-status.log
-log udp.log
-verb 2
+verb 3
 ncp-disable
 cipher none
 auth none
@@ -181,10 +176,17 @@ cp /etc/openvpn/client-tcp-1194.ovpn /home/vps/public_html/client-tcp-1194.ovpn
 # Copy config OpenVPN client ke home directory root agar mudah didownload ( UDP 2200 )
 cp /etc/openvpn/client-udp-2200.ovpn /home/vps/public_html/udp.ovpn
 
-cd
-wget https://github.com/LolLloLlLolLlLolL-rgb/nyamuk/blob/beta/plugin.tgz
-tar -xzvf /root/plugin.tgz -C /etc/openvpn/
-rm -f plugin.tgz
+sed -i "s|#AUTOSTART="all"|AUTOSTART="all"|g" /etc/default/openvpn
+
+# restart openvpn dan cek status openvpn
+systemctl enable --now openvpn-server@server-tcp-1194
+systemctl enable --now openvpn-server@server-udp-2200
+/etc/init.d/openvpn restart
+/etc/init.d/openvpn status
+
+# aktifkan ip4 forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
 iptables -t nat -I POSTROUTING -s 192.168.10.0/24 -o $ANU -j MASQUERADE
 iptables -t nat -I POSTROUTING -s 192.168.11.0/24 -o $ANU -j MASQUERADE
