af48f61813c14765dc5f1d0b2e1666c0235c51a0
PHC Dark  Boy <87409181+LolLloLlLolLlLolL-rgb@users.noreply.github.com>
2021-08-06 02:24:25 +0800

    Update ins-vt.sh

--- ins-vt.sh
+++ ins-vt.sh
@@ -413,7 +413,7 @@ cat <<EOF > /etc/trojan/config.json
         "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384",
         "prefer_server_cipher": false,
         "alpn": [
-            "h2"
+            "h2",
             "http/1.1"
         ],
         "reuse_session": true,
