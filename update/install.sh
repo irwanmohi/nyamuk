676e3ce42b182c5232c1c265d4f3fc5b3922c811
mohd irwan bin mohi <48343630+irwanmohi@users.noreply.github.com>
2021-08-10 14:34:42 +0800

    Update install.sh

--- update/install.sh
+++ update/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd /usr/bin
-wget -q bannerku "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/bannerku"
-wget -q -O menu "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/menu.sh"
-wget -q -O bbr "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/bbr.sh"
-wget -q -O ssh "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/ssh.sh"
-wget -q -O ssssr "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/ssssr.sh"
-wget -q -O trojaan "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/trojaan.sh"
-wget -q -O v2raay "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/v2raay.sh"
-wget -q -O vleess "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/vleess.sh"
-wget -q -O wgr "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/update/wgr.sh"
+wget -q bannerku "https://raw.githubusercontent.com/irwanmohi/nyamuk/beta/update/bannerku"
+wget -q -O menu "https://raw.githubusercontent.com/irwanmohi/nyamuk/beta/update/menu.sh"
+wget -q -O bbr "https://raw.githubusercontent.com/irwanmohi/nyamuk/beta/update/bbr.sh"
+wget -q -O ssh "https://raw.githubusercontent.com/irwanmohi/nyamuk/beta/update/ssh.sh"
+wget -q -O ssssr "https://raw.githubusercontent.com/irwanmohi/nyamuk/beta/update/ssssr.sh"
+wget -q -O trojaan "https://raw.githubusercontent.com/irwanmohi/nyamuk/beta/update/trojaan.sh"
+wget -q -O v2raay "https://raw.githubusercontent.com/irwanmohi/nyamuk/beta/update/v2raay.sh"
+wget -q -O vleess "https://raw.githubusercontent.com/irwanmohi/nyamuk/beta/update/vleess.sh"
+wget -q -O wgr "https://raw.githubusercontent.com/irwanmohi/nyamuk/beta/update/wgr.sh"
 chmod +x bannerku && sed -i -e 's/\r$//' bannerku
 chmod +x menu && sed -i -e 's/\r$//' menu
 chmod +x bbr && sed -i -e 's/\r$//' bbr
